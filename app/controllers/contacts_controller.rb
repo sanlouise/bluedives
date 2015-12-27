031dfe8472bcd4d00475289842b1ae0c5c854aa9
Sandra Adams <sandra.adamshallie@gmail.com>
2015-12-27 20:34:18 +0000

    Update contact files for email

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,30 +1,31 @@
 class ContactsController < ApplicationController
-    def new
-        @contact = Contact.new
-        
-    end
-
-    def create
-        @contact = Contact.new(contact_params)
-        
-        if @contact.save
-            name = params [:contact][:name]
-            email = params [:contact][:email]
-            body  = params [:contact][:body]
-            
-            ContactMailer.contact_email(name, email, body).deliver
-            flash[:success] = 'Message sent!'
-            redirect_to new_contact_path
-        else
-            flash[:danger] = 'Message has not been sent. Please check if you have filled out the form correctly.'
-            redirect_to new_contact_path
-        end
+  def new
+    @contact = Contact.new
+  end
+  
+  def create
+    @contact = Contact.new(contact_params)
+    
+    if @contact.save
+      name = params[:contact][:name]
+      email = params[:contact][:email]
+      body = params[:contact][:comments]
+      
+      ContactMailer.contact_email(name, email, body).deliver
+      
+      flash[:success] = 'Message sent.'
+      redirect_to new_contact_path
+    else
+      flash[:danger] = 'Error occured, message has not been sent.'
+      redirect_to new_contact_path
     end
     
-    
-    private
+  end
+  
+  private
     def contact_params
-        params.require(:contact).permit(:name, :email, :comments)
+      # "white listing" these parameters
+      params.require(:contact).permit(:name, :email, :comments)
     end
-end
+end  
 
