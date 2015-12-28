19266210cb468db9cc3e4c2882cf753f138c1ba8
Sandra Adams <sandra.adamshallie@gmail.com>
2015-12-28 20:38:53 +0000

    Fixed typos in application controller

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,6 @@
 class Users::RegistrationsController < Devise:: RegistrationsController
   def create
-    super do|resource|
+    super do |resource|
       if params[:plan]
         resource.plan_id = params[:plan]
         if resource.plan_id == 2
