a0b2370012ceb3c1c1340fd365f573299224b4b5
Sandra Adams <sandra.adamshallie@gmail.com>
2015-12-30 16:40:34 +0000

    Improved users index page

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,6 +1,8 @@
+
 class ProfilesController < ApplicationController
   before_action :authenticate_user!
   before_action :only_current_user
+  
   def new
     # form where a user can fill out their own profile.
     @user = User.find( params[:user_id] )
@@ -20,30 +22,29 @@ class ProfilesController < ApplicationController
   end
   
   def edit
-    @user = User.find( params[:user_id])
+    @user = User.find( params[:user_id] )
     @profile = @user.profile
   end
-    
+  
   def update
-    @user = User.find( params[:user_id])
+    @user = User.find( params[:user_id] )
     @profile = @user.profile
     if @profile.update_attributes(profile_params)
-      flash[:success] = "Your profile is updated!"
+      flash[:success] = "Profile Updated!"
       redirect_to user_path( params[:user_id] )
     else
       render action: :edit
     end
   end
-    
-    
-
+  
   private
     def profile_params
       params.require(:profile).permit(:first_name, :last_name, :avatar, :job_title, :phone_number, :contact_email, :description)
     end
     
     def only_current_user
-      @user = User.find( params[:user_id])
+      @user = User.find( params[:user_id] )
       redirect_to(root_url) unless @user == current_user
     end
 end
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,16 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-    belongs_to :plan
-    has_one :profile
-    attr_accessor :stripe_card_token
-    
-    def save_with_payment
-      if valid?
-        customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
-        self.stripe_customer_token = customer.id
-        save!
-      
-      end
+  belongs_to :plan
+  has_one :profile
+  attr_accessor :stripe_card_token
+  
+  def save_with_payment
+    if valid?
+      customer = Stripe::Customer.create(description: email, plan: plan_id, card: stripe_card_token)
+      self.stripe_customer_token = customer.id
+      save!
     end
+  end
 end
+
