7f666202a7dac3ef5cb1e5a159ab373929781b2c
Sandra Adams <sandra.adamshallie@gmail.com>
2016-01-04 13:47:48 +0000

    Check in with project

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,4 @@
-class Users::RegistrationsController < Devise:: RegistrationsController
+class Users::RegistrationsController < Devise::RegistrationsController
   before_filter :select_plan, only: :new
   
   def create
@@ -16,7 +16,7 @@ class Users::RegistrationsController < Devise:: RegistrationsController
   
   private
     def select_plan
-      unless params[:plan] && (params[:plan] == '1'|| params[:plan] == '2')
+      unless params[:plan] && (params[:plan] == '1' || params[:plan] == '2')
         flash[:notice] = "Please select a membership plan to sign up."
         redirect_to root_url
       end
