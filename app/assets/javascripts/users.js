ed48230b1fadca1e6d96f07ea3af876a7e62c633
Sandra Adams <sandra.adamshallie@gmail.com>
2016-02-05 12:31:05 +0000

    Update environments for production

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -9,7 +9,7 @@ $(document).ready(function() {
         cvcNum = $('#card_code').val(),
         expMonth = $('#card_month').val(),
         expYear = $('#card_year').val();
-
+        
     if (!error) {
       // Get the Stripe token:
       Stripe.createToken({
@@ -25,14 +25,14 @@ $(document).ready(function() {
   function stripeResponseHandler(status, response) {
     // Get a reference to the form:
     var f = $("#new_user");
-
+    
     // Get the token from the response:
     var token = response.id;
-
+    
     // Add the token to the form:
     f.append('<input type="hidden" name="user[stripe_card_token]" value="' + token + '" />');
-
-    // Submit the form:
-    f.get(0).submit(); 
+    
+    // Submit the  form:
+    f.get(0).submit();
   }
 });
