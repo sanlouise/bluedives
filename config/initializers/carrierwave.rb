774dae796fa3b5dfee14107ee4af451fa7e98f48
Sandra Adams <sandra.adamshallie@gmail.com>
2016-02-06 15:47:49 +0000

    Update AWS3

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,16 @@
+CarrierWave.configure do |config|
+  config.root = Rails.root.join('tmp') # adding these...
+  config.cache_dir = 'carrierwave' # ...two lines
+
+  config.fog_credentials = {
+    :provider               => 'AWS',                        # required
+    :AWS_ACCESS_KEY_ID      => ENV['S3_ACCESS_KEY'],                        # required
+    :AWS_SECRET_ACCESS_KEY  => ENV['S3_SECRET_KEY'],                     # required
+    :region                 => 'eu-central-1',                  # optional, defaults to 'us-east-1'
+    :host                   => 's3.example.com',             # optional, defaults to nil
+    :endpoint               => 'https://s3.example.com:8080' # optional, defaults to nil
+  }
+  config.fog_directory  = ENV['S3_Bucket']                             # required
+  config.fog_public     = false                                   # optional, defaults to true
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}  # optional, defaults to {}
+end
